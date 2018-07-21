58c054e3370022a7fa4816c7219bcc474ba99cf3
Arjun <arjun10051992@gmail.com>
2018-07-21 21:10:42 +0530

    JavaImportant"ProgramsPractice"

---
+++ src/main/java/interview/question/programs/PrintingPattern.java
@@ -0,0 +1,75 @@
+package interview.question.programs;
+
+public class PrintingPattern {
+
+	public static void main(String[] args) {
+
+		/*for (int i = 0; i <=5; i++) {
+			for (int j = 0; j <i; j++) {// printing *
+				System.out.print("*");
+			}						
+			System.out.println("");
+		}
+*/
+		/*for (int i = 0; i <=5; i++) {
+			for (int j = 1; j <=i; j++) {
+				System.out.print(j+" ");//printing trangle 1 12 123 
+			}
+			System.out.println("");
+		}*/
+	/*	for (int i = 1; i <=5; i++) {
+		for (int j = 1; j <=i; j++) {
+			System.out.print(i+" ");// priting traingle 1 2 2 333 4444
+		}
+		System.out.println("");
+	}*/
+		
+		int i,k,j;
+		for ( i = 1; i <=5; i++) 
+		{
+			for (j = i; j <5; j++)
+				System.out.print(" ");
+			for ( k = 1; k <(i*2); k++) 
+				System.out.print("*");	
+			System.out.println("");
+		}
+		for ( i =4; i>=1; i--)  
+		{
+			for (j =5; j>i; j--)
+				System.out.print(" ");
+			for ( k = 1; k <(i*2); k++) 
+				System.out.print("*");	
+			System.out.println("");
+		}
+		/*int number,i,k;
+		int count=1;
+		number=5;
+		count=number-1;
+		for (k = 1; k <=number; k++) {
+			for ( i = 1; i <=count; i++) 
+				System.out.print(" ");
+				count--;
+				for ( i = 1; i <2*k-1; i++) 
+					System.out.print("*");	
+				System.out.println(" ");
+				}
+				count=1;
+				for ( k = 1; k <number-1; k++) {
+					for ( i = 1; i <=count; i++) 
+						System.out.print(" ");
+						count++;
+						for ( i = 1; i <2*(number-k)-1; i++) 
+							System.out.print("*");
+							System.out.println(" ");
+						
+				}*/
+	}
+		/*int i,j,k=1;
+		for ( i = 1; i <=5; i++) {
+		for ( j = 1; j <i; j++) {
+			System.out.print(k++ +" ");
+		}			
+		System.out.println(" ");*/
+	}
+	
+
---
+++ src/main/java/interview/question/programs/StringPrograms.java
@@ -0,0 +1,114 @@
+package interview.question.programs;
+
+public class StringPrograms {
+
+	public static void main(String[] args) {
+		
+		System.out.println("converting string to integer ");
+		
+		String s="100";
+		int i=Integer.parseInt(s);
+        System.out.println(i);
+        
+        System.out.println("converting integer to string ");
+         int j=100;
+         String s1=String.valueOf(j);
+         System.out.println(s1);
+        
+         System.out.println("converting string to float ");
+       
+		float f=Float.parseFloat(s);
+		System.out.println(f);
+		//float to string
+		String s3=String.valueOf(f);
+		System.out.println(s3);
+		
+		System.out.println("converting string to double ");
+		double d=Double.parseDouble(s);
+		System.out.println(d);
+		//double to string
+		String s4=String.valueOf(d);
+		System.out.println(s4);
+		
+		// String to boolean
+		
+		System.out.println("converting string to boolean ");
+		boolean b=Boolean.parseBoolean(s);
+		System.out.println(b);
+		
+		// boolean to string
+		boolean tr=true;
+		String s2=Boolean.toString(tr);
+		System.out.println(s2);
+		
+		//converting string to long
+		long lg=Long.parseLong(s);
+		System.out.println(lg);
+		
+		//long to string
+		String s5=String.valueOf(lg);
+		System.out.println(s5);
+		
+	//String to byte,byte to string there is no method 
+	byte by=Byte.parseByte(s);
+     System.out.println(by);
+    
+   	
+	// converting char to string	// string to char not possible there is method in java
+		char c='a';
+		String s7=String.valueOf(c);
+		System.out.println(s7);	
+		
+		String s8="This as arjan arom qa";
+		
+		System.out.println(s8.length());
+		System.out.println(s8.charAt(3));
+		System.out.println(s8.substring(0, 10));
+		System.out.println(s8.substring(1));
+		System.out.println(s8.indexOf("i"));
+		System.out.println(s8.lastIndexOf("a"));
+		
+		//String s8="This as arjan arom qa";
+		//System.out.println(s8.indexOf("a"));//it will print first a 
+		//System.out.println(s8.indexOf('a', s8.indexOf('a')+1));// it will print 2nd a
+		//System.out.println(s8.indexOf('a', s8.indexOf('a', s8.indexOf('a')+1)+1));// it will print 3rd a
+		//System.out.println(s8.indexOf('a',s8.indexOf('a', s8.indexOf('a', s8.indexOf('a')+1)+1)+1));// it will print 4th a
+		//System.out.println(s8.indexOf('a',s8.indexOf('a',s8.indexOf('a', s8.indexOf('a', s8.indexOf('a')+1)+1)+1)+1));// it will print last a for last letter we have another method lastIndexOf(args);
+		
+		int firstA=s8.indexOf("a");
+		System.out.println(firstA);
+		
+		int secondA=s8.indexOf('a', firstA+1);
+		System.out.println(secondA);
+		
+		int thirdA=s8.indexOf('a', secondA+1);
+		System.out.println(thirdA);
+		
+		int forthA=s8.indexOf('a', thirdA+1);
+		System.out.println(forthA);
+		
+		int fifthA=s8.indexOf('a', forthA+1);
+		System.out.println(fifthA);
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
